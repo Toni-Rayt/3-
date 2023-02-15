f1f84227c92fd5383751975906188c673c234e40
Toni-Rayt <121283960+Toni-Rayt@users.noreply.github.com>
2023-02-15 16:05:42 +0300

    Добавьте файлы проекта.

---
+++ 3кнопки/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3кнопки
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form2 = new odnomer();
+            form2.Closed += (s, args) => this.Close();
+            form2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form2 = new dvymer();
+            form2.Closed += (s, args) => this.Close();
+            form2.Show();
+        }
+    }
+}
